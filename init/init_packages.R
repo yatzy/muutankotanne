9986fc369da9c6c3f039ac637eee99b809c86035
teemumeronen <teemu.meronen@gmail.com>
2014-12-22 21:09:09 +0200

    init tiedostoon lisätty networkD3

--- init/init_packages.R
+++ init/init_packages.R
@@ -10,7 +10,7 @@ use_base_packages = function(package_list){
    sapply(package_list , use_base_package)
 }
 
-use_package('devtools')
+use_base_package('devtools')
 
 use_github_package <- function(package) {
    if (!is.element(package, installed.packages()[,1]))
@@ -24,10 +24,11 @@ use_github_packages = function(package_list){
 
 ############# ADD NEEDED PACKAGES TO THIS LIST
 base_libraries = c('httr' , 'RCurl' ,'XML' , 'ggplot2' , 'maps' 
-              ,'jsonlite' ,'leafletR')
-use_packages(base_libraries)
+              ,'jsonlite' ,'leafletR', 'networkD3')
+use_base_packages(base_libraries)
+
+
 
 ############# ADD NEEDED GITHUB PACKAGES TO THIS LIST
-github_libraries = c('jcheng5/leaflet-shiny' , 'trestletech/ShinyDash')
-use_github_packages(github_libraries)
+
 
