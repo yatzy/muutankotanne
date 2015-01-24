004ff286b5199079082ab94751afd5c211a229e5
Juha <jyatzy@gmail.com>
2015-01-24 20:57:51 +0200

    asfd

--- App/init/init_packages.R
+++ App/init/init_packages.R
@@ -22,13 +22,17 @@ use_github_packages = function(package_list){
 }
 
 ############# ADD NEEDED PACKAGES TO THIS LIST
-base_libraries = c('shiny','httr' , 'RCurl' ,'XML' , 'ggplot2' , 'maps' , 'stringr'
-              ,'jsonlite' ,'htmlwidgets' ,'magrittr', 'networkD3' 
-              , 'pbapply', 'scrapeR' ,'repmis' , 'tm.plugin.webmining','rjson'
-              , 'stringr', 'rvest','stringi','RColorBrewer','scales','lattice'
-              , 'ShinyDash')
-# no ,'leafletR' ,'dplyr'
-use_base_packages(base_libraries)
+shiny_libraries = c('shiny' , 'jsonlite')
+use_base_packages(shiny_libraries)
+
+# ############# ADD NEEDED PACKAGES TO THIS LIST
+# base_libraries = c('shiny','httr' , 'RCurl' ,'XML' , 'ggplot2' , 'maps' , 'stringr'
+#               ,'jsonlite' ,'htmlwidgets' ,'magrittr', 'networkD3' 
+#               , 'pbapply', 'scrapeR' ,'repmis' , 'tm.plugin.webmining','rjson'
+#               , 'stringr', 'rvest','stringi','RColorBrewer','scales','lattice'
+#               , 'ShinyDash')
+# # no ,'leafletR' ,'dplyr'
+# use_base_packages(base_libraries)
 
 ############# ADD NEEDED GITHUB PACKAGES TO THIS LIST
 github_libraries = c( 'trestletech/ShinyDash' #,'jcheng5/leaflet-shiny'
