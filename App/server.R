6ca5d33f828b807edfa8725bbc37f9cf66e4a4a0
Juha <jyatzy@gmail.com>
2015-01-24 19:52:49 +0200

    server inited

--- App/server.R
+++ App/server.R
@@ -1,26 +1,43 @@
-library(shiny)
-library(leaflet)
-library(RColorBrewer)
-library(scales)
-library(lattice)
-library(dplyr)
+if(file.exists("/home/yatzy/Applications/muutankotanne/App/")==TRUE){
+   setwd("/home/yatzy/Applications/muutankotanne/App/")
+} else if( file.exists("/home/kurkku/Applications/muutankotanne/App/")==TRUE ){
+   setwd('/home/kurkku/Applications/muutankotanne/App/')
+}
 
-# Leaflet bindings are a bit slow; for now we'll just sample to compensate
-set.seed(100)
-#zipdata <- allzips[sample.int(nrow(allzips), 10000),]
-# By ordering by centile, we ensure that the (comparatively rare) SuperZIPs
-# will be drawn last and thus be easier to see
-#zipdata <- zipdata[order(zipdata$centile),]
+source('init/init_packages.R')
+source('AlueMap/aluemap.R')
 
+# parameters
+DEBUG =T
+MARKERS=T
+
+
+# https://github.com/jcheng5/leaflet-shiny/blob/polygon-example/inst/example/server.R
 ########### parametrit
+
+
+bindEvent <- function(eventExpr, callback, env=parent.frame(), quoted=FALSE) {
+   eventFunc <- exprToFunction(eventExpr, env, quoted)
+   
+   initialized <- FALSE
+   invisible(observe({
+      eventVal <- eventFunc()
+      if (!initialized)
+         initialized <<- TRUE
+      else
+         isolate(callback())
+   }))
+}
+
 
 
 shinyServer(function(input, output, session) {
    
-   ## Interactive Map ###########################################
+   ########## Interactive Map #################################
    
    # Create the map
    map <- createLeafletMap(session, "map")
+   
    
    output$kotiosoite <- renderText({ paste( input$kotiosoite_from_ui ) })
    output$muutto_osoite <- renderPrint({ cat(input$muutto_osoite_from_ui) })
@@ -28,6 +45,63 @@ shinyServer(function(input, output, session) {
    output$koti_pic = renderPlot(plot(1:10))
    output$muutto_pic = renderPlot(plot(10:1))
    
-   koti_coordinates = 
+   koti_coord = reactive({reverse_geocode_nominatim( input$kotiosoite_from_ui )[c('lat','lon')] })
    
+   output$kotiosoite_coord = renderPrint( reverse_geocode_nominatim( input$kotiosoite_from_ui )[c('lat','lon')] )
+   output$muutto_osoite_coord = renderPrint(reverse_geocode_nominatim( input$muutto_osoite_from_ui )[c('lat','lon')] )   
+   
+   if(MARKERS){
+      ##################### markers #####################
+      
+      values <- reactiveValues(markers = NULL)
+      
+      bindEvent(input$map_click, function() {
+         values$selectedCity <- NULL
+         if (!input$addMarkerOnClick)
+            return()
+         map$addMarker(input$map_click$lat, input$map_click$lng, NULL)
+         values$markers <- rbind(data.frame(lat=input$map_click$lat,
+                                            long=input$map_click$lng),
+                                 values$markers)
+      })
+      
+      bindEvent(input$clearMarkers, function() {
+         map$clearMarkers()
+         values$markers <- NULL
+      })
+           
+      output$markers <- renderTable({
+         if (is.null(values$markers))
+            return(NULL)
+         
+         data <- values$markers
+         colnames(data) <- c('Latitude', 'Longitude')
+         return(data)
+      })
+      
+      #################### markers removal   #################### 
+      
+      bindEvent(input$clearMarkers, function() {
+         map$clearMarkers()
+         values$markers <- NULL
+      })
+      
+      
+      bindEvent(input$map_shape_click, function() {
+         event <- input$map_shape_click
+         map$clearPopups()
+         
+         cities <- topCitiesInBounds()
+         city <- cities[row.names(cities) == event$id,]
+         values$selectedCity <- city
+         content <- as.character(tagList(
+            tags$strong(paste(city$City, city$State)),
+            tags$br(),
+            sprintf("Estimated population, %s:", input$year),
+            tags$br(),
+            prettyNum(city[[popCol()]], big.mark=',')
+         ))
+         map$showPopup(event$lat, event$lng, content, event$id)
+      })
+   }
 })
